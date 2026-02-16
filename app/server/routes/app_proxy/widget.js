5ccc31a3fa9ef573a1d6a04888f4e14989233414
AnitBishwas <anit.biswas@swissbeauty.in>
2026-02-17 01:36:43 +0530

    Customer wallet route added

--- app/server/routes/app_proxy/widget.js
+++ app/server/routes/app_proxy/widget.js
@@ -1,8 +1,28 @@
 import { Router } from "express";
 import { createWalletWidget } from "../../controllers/widget.js";
+import WalletModel from "../../../utils/models/Wallet.js";
 
 const widgetRoutes = Router();
 
+widgetRoutes.get("/balance", async (req, res) => {
+  try {
+    const customerId = req.query.logged_in_customer_id;
+    const customerWallet = await WalletModel.findOne({
+      customerId: customerId,
+    }).lean();
+    if (!customerWallet) {
+      throw new Error("No wallet found against customer");
+    }
+    res.status(200).json({
+      ok: true,
+      balance: customerWallet.balance,
+    });
+  } catch (err) {
+    res.status(400).json({
+      ok: false,
+    });
+  }
+});
 widgetRoutes.get("/", async (req, res) => {
   try {
     const customerId = req.query.logged_in_customer_id;
