825fa14bdf5a83345bb42b640208e1f8ea7871ea
AnitBishwas <anit.biswas@swissbeauty.in>
2026-02-16 17:23:07 +0530

    Navigation added on homepage

--- app/client/pages/Index.jsx
+++ app/client/pages/Index.jsx
@@ -2,16 +2,37 @@ import {
   BlockStack,
   Button,
   Card,
+  InlineGrid,
   InlineStack,
   Layout,
   Page,
   Text,
 } from "@shopify/polaris";
 import { ExternalIcon } from "@shopify/polaris-icons";
-import { navigate } from "raviger";
+import { Link, navigate } from "raviger";
 
 const HomePage = () => {
-  return <></>;
+  return (
+    <Page>
+      <InlineGrid columns={2} gap={200}>
+        <Card>
+          <Link variant="breadcrumb" href="/customer-wallet">Customer wallet</Link>
+        </Card>
+        <Card>
+          <Link href="/discounts">Manage Discounts</Link>
+        </Card>
+        <Card>
+          <Link href="/distribution">Manage Distribution</Link>
+        </Card>
+        <Card>
+          <Link href="/storefront-offers">Storefront offers</Link>
+        </Card>
+        <Card>
+          <Link href="/settings">Settings</Link>
+        </Card>
+      </InlineGrid>
+    </Page>
+  );
 };
 
 export default HomePage;
